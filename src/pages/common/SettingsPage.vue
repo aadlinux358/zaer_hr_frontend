e11619cd19ffa0519be603bb12a24b19f8cd287f
Abel Alazar Dirar <aadlinux358@gmail.com>
2023-05-20 18:48:21 +0300

    add: settings page

---
+++ src/pages/common/SettingsPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-toggle v-model="uiStore.dense"
+            checked-icon="check"
+            color="primary"
+            label="Dense mode"
+            unchecked-icon="clear" />
+</template>
+<script setup lang="ts">
+import {useStores} from 'src/composables/stores';
+const {uiStore} = useStores();
+</script>
