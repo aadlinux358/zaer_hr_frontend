98e5cd902cfecba84aecad0dabfe21c78088cf19
Abel Alazar Dirar <aadlinux358@gmail.com>
2023-05-20 22:51:28 +0300

    add: form validation

--- src/composables/forms.ts
+++ src/composables/forms.ts
@@ -4,7 +4,7 @@ import {CRUDType} from 'src/models/common';
 export function useForms<T>(props, emits) {
 
   const form: Ref<T> = ref({});
-
+  const formRef = ref(null);
   const crudType = ref(CRUDType.CREATE)
   onMounted(() => {
     if (props.payload) {
@@ -14,11 +14,17 @@ export function useForms<T>(props, emits) {
   })
   onUnmounted(() => form.value = {})
 
-  function onCreate() {
-    emits('create', form.value);
+  async function onCreate() {
+    const isValid = await formRef.value.validate()
+    if (isValid) {
+      emits('create', form.value);
+    }
   }
-  function onUpdate() {
-    emits('update', form.value)
+  async function onUpdate() {
+    const isValid = await formRef.value.validate()
+    if (isValid) {
+      emits('update', form.value)
+    }
   }
   function onReset() {
     form.value = {};
@@ -30,6 +36,7 @@ export function useForms<T>(props, emits) {
   return {
     crudType,
     form,
+    formRef,
     onCreate,
     onUpdate,
     onReset,
