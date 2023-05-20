51872111f364883df462a51c080a066a89b086ae
Abel Alazar Dirar <aadlinux358@gmail.com>
2023-05-20 18:47:07 +0300

    add: employee form and detail routes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -19,47 +19,63 @@ const routes: RouteRecordRaw[] = [
       {
         path: '/dashboard',
         name: 'Index',
-        component: () => import('pages/main/IndexPage.vue'),
+        component: () => import('src/pages/main/IndexPage.vue'),
       },
       {
         path: '/employees',
         name: 'Employees',
-        component: () => import('pages/main/EmployeePage.vue'),
+        component: () => import('src/pages/main/EmployeePage.vue'),
+      },
+      {
+        path: '/employee-form',
+        name: 'EmployeeForm',
+        component: () => import('src/forms/EmployeeForm.vue')
+      },
+      {
+        path: '/employees/:empUid',
+        name: 'EmployeeDetail',
+        component: () => import('src/pages/shared/EmloyeeDetailPage.vue'),
+        props: true
       },
       {
         path: '/divisions',
         name: 'Divisions',
-        component: () => import('pages/main/DivisionPage.vue'),
+        component: () => import('src/pages/main/DivisionPage.vue'),
       },
       {
         path: '/departments',
         name: 'Departments',
-        component: () => import('pages/main/DepartmentPage.vue'),
+        component: () => import('src/pages/main/DepartmentPage.vue'),
       },
       {
         path: '/sections',
         name: 'Sections',
-        component: () => import('pages/main/SectionPage.vue'),
+        component: () => import('src/pages/main/SectionPage.vue'),
       },
       {
         path: '/units',
         name: 'Units',
-        component: () => import('pages/main/UnitPage.vue'),
+        component: () => import('src/pages/main/UnitPage.vue'),
       },
       {
         path: '/designations',
         name: 'Designations',
-        component: () => import('pages/main/DesignationPage.vue'),
+        component: () => import('src/pages/main/DesignationPage.vue'),
       },
       {
         path: '/about',
         name: 'About',
-        component: () => import('pages/common/AboutPage.vue'),
+        component: () => import('src/pages/common/AboutPage.vue'),
       },
       {
         path: '/help',
         name: 'Help',
-        component: () => import('pages/common/HelpPage.vue'),
+        component: () => import('src/pages/common/HelpPage.vue'),
+      },
+      {
+        path: '/settings',
+        name: 'Settings',
+        component: () => import('src/pages/common/SettingsPage.vue')
       },
       {
         path: '',
@@ -96,47 +112,47 @@ const routes: RouteRecordRaw[] = [
       {
         path: 'dashboard',
         name: 'AdminDashboard',
-        component: () => import('pages/admin/AdminIndexPage.vue')
+        component: () => import('src/pages/admin/AdminIndexPage.vue')
       },
       {
         path: 'employees',
         name: 'AdminEmployees',
-        component: () => import('pages/admin/AdminEmployeePage.vue')
+        component: () => import('src/pages/admin/AdminEmployeePage.vue')
       },
       {
         path: 'divisions',
         name: 'AdminDivisions',
-        component: () => import('pages/admin/AdminDivisionPage.vue')
+        component: () => import('src/pages/admin/AdminDivisionPage.vue')
       },
       {
         path: 'departments',
         name: 'AdminDepartments',
-        component: () => import('pages/admin/AdminDepartmentPage.vue')
+        component: () => import('src/pages/admin/AdminDepartmentPage.vue')
       },
       {
         path: 'sections',
         name: 'AdminSections',
-        component: () => import('pages/admin/AdminSectionPage.vue')
+        component: () => import('src/pages/admin/AdminSectionPage.vue')
       },
       {
         path: 'units',
         name: 'AdminUnits',
-        component: () => import('pages/admin/AdminUnitPage.vue')
+        component: () => import('src/pages/admin/AdminUnitPage.vue')
       },
       {
         path: 'designations',
         name: 'AdminDesignations',
-        component: () => import('pages/admin/AdminDesignationPage.vue')
+        component: () => import('src/pages/admin/AdminDesignationPage.vue')
       },
       {
         path: 'nationalities',
         name: 'AdminNationalities',
-        component: () => import('pages/admin/AdminNationalityPage.vue')
+        component: () => import('src/pages/admin/AdminNationalityPage.vue')
       },
       {
         path: 'educational-levels',
         name: 'AdminEducationalLevels',
-        component: () => import('pages/admin/AdminEducationalLevelPage.vue')
+        component: () => import('src/pages/admin/AdminEducationalLevelPage.vue')
       }
 
     ]
@@ -144,14 +160,14 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/login',
     name: 'Login',
-    component: () => import('pages/common/LoginPage.vue'),
+    component: () => import('src/pages/common/LoginPage.vue'),
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
     path: '/:catchAll(.*)*',
-    component: () => import('pages/common/ErrorNotFound.vue'),
+    component: () => import('src/pages/common/ErrorNotFound.vue'),
   },
 
 ];
