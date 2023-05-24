424281c17d2e6c921ecdf82a1cee4d60c9502172
Abel Alazar Dirar <aadlinux358@gmail.com>
2023-05-24 09:32:37 +0300

    add: employee detail actions component

---
+++ src/components/EmployeeDetailActions.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const emits = defineEmits(['edit', 'terminate', 'deactivate',])
+</script>
+<template>
+  <div class="row q-gutter-sm q-ma-sm ">
+    <q-btn :label="$t('edit')"
+           square
+           no-caps
+           class="col text-capitalize"
+           color="primary"
+           icon="edit"
+           @click="emits('edit')" />
+    <q-btn :label="$t('deactivate')"
+           square
+           no-caps
+           class="col text-capitalize"
+           color="primary"
+           icon="remove_circle"
+           @click="emits('deactivate')" />
+    <q-btn :label="$t('terminate')"
+           square
+           no-caps
+           class="col text-capitalize"
+           color="primary"
+           icon="clear"
+           @click="emits('terminate')" />
+  </div>
+</template>
